111faf06bd871ad54f542d0e3976e6a777b99924
NanoRocky <76585834+NanoRocky@users.noreply.github.com>
2026-04-25 00:16:17 +0800

    更新

--- XiaoaiAgentInjector.cpp
+++ XiaoaiAgentInjector.cpp
@@ -3,7 +3,7 @@
 int main() {
     return RunInjector(
         "XiaoaiAgent Injector",
-        "SOFTWARE\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\App Paths\\\\XiaoaiAgent.exe",
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\XiaoaiAgent.exe",
         "XiaoaiAgent.exe",
         "错误：未能从小爱注册表获取安装路径，请检查程序是否安装！"
     );
